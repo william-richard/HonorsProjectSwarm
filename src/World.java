147b4df4046282ea115dd5796cea4800a2253ad2
Will <Will@themacofwill-2.campus.bowdoin.edu>
2010-06-21 11:41:46 -0400

    First commit - nothing really done yet.

---
+++ src/World.java
@@ -0,0 +1,19 @@
+import java.awt.Rectangle;
+
+
+public class World extends Rectangle {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
