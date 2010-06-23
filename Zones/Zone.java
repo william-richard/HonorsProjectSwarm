d18411ddcdb2ee8c6bc92915bd913e076b8fe075
Will <Will@139.140.170.233>
2010-06-23 10:25:29 -0400

    Started implimenting Zone

--- Zones/Zone.java
+++ Zones/Zone.java
@@ -1,8 +1,20 @@
+import java.awt.Color;
 import java.awt.Polygon;
 
 
 public abstract class Zone extends Polygon {
 
 	private static final long serialVersionUID = 1L;
+	
+	private Color zoneColor;
+	
+	
+	public Zone(int[] xPoints, int[] yPoints, int numPoints) {
+		super(xPoints, yPoints, numPoints);
+	}
+	
+	public Color getZoneColor() {
+		return zoneColor;
+	}
 
 }
