91c34ab83bc2955b9c6dc70710180c6512549272
William Richard <willster3021@gmail.com>
2011-04-25 18:18:56 -0400

    Created script to convert screenshot files to movies

---
+++ Create_screenshot_movies.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mydir=$(dirname $(which $0))
+cd $mydir
+cd ./data
+
+#go through each folder i.e. survivors
+for sur in $(find . -type d -depth 1)
+do
+    cd $sur
+    #iterate through folders of num bots
+    for bot in $(find . -type d -depth 1)
+    do
+	cd $bot
+	#iterate through folders with individual runs
+	for run in $(find . -type d -depth 1)
+	do
+	    cd $run
+
+	    #see if the screenshot folder exists
+	    if [ -d "screenshots" ]; then
+		#if it does, create the movie
+		ffmpeg -f image2 -r 2 -i ./screenshots/%d.jpeg -b 600k ./replay.mp4
+		#remove the screenshots folder
+		rm -r ./screenshots
+	    fi
+
+	    cd ..
+	    #done with run loop
+	done
+	cd ..
+	#done with bot loop
+    done
+    cd ..
+    #done with sur loop
+done
