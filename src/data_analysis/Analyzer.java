4ee53cd45220909012931204ef921d5d0c672ca7
William Richard <willster3021@gmail.com>
2011-04-28 00:08:06 -0400

    Added Data Analysis classes

---
+++ src/data_analysis/Analyzer.java
@@ -0,0 +1,135 @@
+package data_analysis;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Analyzer {
+
+	public static void main(String[] args) {
+		//args should be the location of the data directory
+		if(args.length != 1) {
+			System.out.println("Incorrect number of arguments passed");
+			System.exit(0);
+		}
+
+		//make sure the passed string is a valid directory
+		File dataDir = new File(args[0]);
+
+		if(! dataDir.isDirectory()) {
+			System.out.println("Argument is not a directory path");
+			System.exit(0);
+		}
+
+		//make sure the directory is called "data"
+		if(! dataDir.getName().equals("data")) {
+			System.out.println("Directory passed does not have the correct name");
+			System.exit(0);
+		}
+
+		//now we can start the traversal
+		NumberDirFileFilter numFileFilter = new NumberDirFileFilter();
+		File[] surDirs = dataDir.listFiles(numFileFilter);
+
+		for(File curSurDir : surDirs) {
+			int curNumSur = Integer.parseInt(curSurDir.getName());
+			//get their Bot sub directories
+			File[] botDirs = curSurDir.listFiles(numFileFilter);
+			
+			for(File curBotDir : botDirs) {
+				int curNumBots = Integer.parseInt(curBotDir.getName());
+				
+				//each sub directory here should be a Run
+				//don't bother checking name validity - not worth it
+				//TODO check for valid dir names
+				File[] runDirs = curBotDir.listFiles(new FileFilter() {
+					@Override
+					public boolean accept(File pathname) {
+						return pathname.isDirectory();
+					}
+				});
+				
+				System.out.println("Staring to analyze sur = "+ curNumSur + " bots = " + curNumBots);
+				
+				List<Run> runs = new ArrayList<Run>();
+				
+				for(File curRunDir : runDirs) {
+					runs.add(Run.readFromFile(new File(curRunDir,"data.txt"), curNumSur, curNumBots));
+				}
+				
+				//we should now have all runs for the given number of bots and survivors
+				//start creating our averages file
+				try {		
+					/* Note To Self: data column assignments
+					 * 0 : timestep
+					 * 1 : Start time in seconds since 1970
+					 * 2 : % sur found
+					 * 3 : Path quality metric
+					 * 4 : Path Marking Metric
+					 * 5 : Overall Metric
+					 */
+
+					/* Output column assignments
+					 * 0 : timestep
+					 * 1 : average value
+					 * 2 : low value
+					 * 3 : high value
+					 */
+					
+					BufferedWriter surFoundOut = new BufferedWriter(new FileWriter(new File(curBotDir, "surFound.dat"), false));
+					BufferedWriter pathQualOut = new BufferedWriter(new FileWriter(new File(curBotDir, "pathQal.dat"), false));
+					BufferedWriter pathMarkOut = new BufferedWriter(new FileWriter(new File(curBotDir, "pathMark.dat"), false));
+					BufferedWriter overallOut  = new BufferedWriter(new FileWriter(new File(curBotDir, "overall.dat"), false));
+					
+					for(int timestep = 0; timestep < runs.get(0).getNumRows(); timestep++) {
+						surFoundOut.write(timestep + "\t" + Run.getAvg(runs, timestep, 2) + "\t" + Run.getMin(runs,timestep, 2) + "\t" + Run.getMax(runs, timestep, 2));
+						surFoundOut.newLine();
+						
+						pathQualOut.write(timestep + "\t" + Run.getAvg(runs, timestep, 3) + "\t" + Run.getMin(runs,timestep, 3) + "\t" + Run.getMax(runs, timestep, 3));
+						pathQualOut.newLine();
+						
+						pathMarkOut.write(timestep + "\t" + Run.getAvg(runs, timestep, 4) + "\t" + Run.getMin(runs,timestep, 4) + "\t" + Run.getMax(runs, timestep, 4));
+						pathMarkOut.newLine();
+						
+						overallOut.write(timestep + "\t" + Run.getAvg(runs, timestep, 5) + "\t" + Run.getMin(runs,timestep, 5) + "\t" + Run.getMax(runs, timestep, 5));
+						overallOut.newLine();
+					}
+					
+					//close up the files
+					surFoundOut.close();
+					pathQualOut.close();
+					pathMarkOut.close();
+					overallOut.close();
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+
+	private static class NumberDirFileFilter implements FileFilter {
+
+		@Override
+		public boolean accept(File pathname) {
+			//we want directories
+			if(! pathname.isDirectory()) return false;
+			//we also want them to have just a number as their name
+			try {
+				Integer.parseInt(pathname.getName());
+			} catch(NumberFormatException e) {
+				return false;
+			}
+			return true;
+		}
+
+	}
+
+
+
+}
---
+++ src/data_analysis/Run.java
@@ -0,0 +1,147 @@
+package data_analysis;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Arrays;
+import java.util.List;
+
+public class Run {
+
+	private double[][] data;
+	private final int numBots;
+	private final int numSurs;
+
+	private Run(int numTimesteps, int numCols, int _numSurs, int _numBots) {
+		data = new double[numTimesteps][numCols];
+		numBots = _numBots;
+		numSurs = _numSurs;
+	}
+
+	public int getNumRows() {
+		return data.length;
+	}
+
+	public int getNumCols() {
+		return data[0].length;
+	}
+
+	public double getValue(int r, int c) {
+		return data[r][c];
+	}
+
+
+	/**
+	 * @return the numBots
+	 */
+	public int getNumBots() {
+		return numBots;
+	}
+
+	/**
+	 * @return the numSurs
+	 */
+	public int getNumSurs() {
+		return numSurs;
+	}
+
+	public void addRow(String row) {
+		//split the string into columns
+		String[] splitRow = row.split("\t");
+
+		//store this row, converting to values
+		int rowNum = Integer.parseInt(splitRow[0]);
+
+		for(int i = 0; i < splitRow.length; i++) {
+			try {
+				data[rowNum][i] = Double.parseDouble(splitRow[i]);
+			} catch (NumberFormatException e) {
+				//can't read this number
+				//put nan
+				data[rowNum][i] = Double.NaN;
+			}
+		}
+	}
+
+	public static Run readFromFile(File file, int numSurs, int numBots) {
+		//read the last line of the file, to get number of timesteps and number of columns
+		Run run = null;
+		try {
+			RandomAccessFile raf = new RandomAccessFile(file, "r");
+			long maxLineLength = 100;
+			long position = raf.length() - maxLineLength;
+			raf.seek(position);
+			String lastLine = null;
+			String s;
+			while((s = raf.readLine()) != null) {
+				lastLine = s;
+			}
+			raf.seek(0);
+			raf.close();
+
+			//lastLine should now be the complete last line of the file
+			//read the relevant data, and make our Run
+			String[] splitLastLine = lastLine.split("\t");
+			System.out.println(file.getAbsolutePath());
+			System.out.println("'" + lastLine + "'");
+			System.out.println(Arrays.toString(splitLastLine));
+			//if we get to timestep 100, there are actually 101 timesteps, because we need to count 0
+			int numTimesteps = Integer.parseInt(splitLastLine[0]) + 1;
+
+			run = new Run(numTimesteps, splitLastLine.length, numSurs, numBots);
+
+			//read each line in order, and add them to the Run
+			BufferedReader read = new BufferedReader(new FileReader(file));
+			String curLine;
+			while( (curLine = read.readLine()) != null) {
+				run.addRow(curLine);
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return run;
+	}
+
+	public static double getMax(List<Run> runs, int row, int col) {
+		double maxVal = runs.get(0).getValue(row, col);
+		for(Run run : runs) {
+			double curVal = run.getValue(row, col);
+			if(curVal > maxVal) 
+				maxVal = curVal;
+		}
+
+		return maxVal;
+	}
+
+
+	public static double getMin(List<Run> runs, int row, int col) {
+		double minVal = runs.get(0).getValue(row, col);
+		for(Run run : runs) {
+			double curVal = run.getValue(row, col);
+			if(curVal < minVal) 
+				minVal = curVal;
+		}
+
+		return minVal;
+	}
+
+	public static double getAvg(List<Run> runs, int row, int col) {
+		double avgVal = 0.0;
+		for(Run run : runs) {
+			avgVal += run.getValue(row, col);
+		}
+		avgVal /= runs.size();
+
+		return avgVal;
+	}
+
+}
