de53b61a743eccf1f41337ef57a005867a044926
William Richard <willster3021@gmail.com>
2011-04-28 08:37:21 -0400

    Added data transfer script

---
+++ transfer_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mydir=$(dirname $(which $0))
+cd $mydir
+cd ./data
+
+#go through each folder i.e. survivors
+for sur in $(find . -type d -depth 1)
+do
+
+#tarFile=$(basename $sur).tar
+
+#echo ./$tarFile
+
+#tar -cf tarFile $sur
+echo $sur
+
+scp -r $sur Will@krishna.student.bowdoin.edu:/Users/Will/Documents/workspace/Swarm/data2/
+
+done
